375809dbf2c4675e6512b4f934cbb64432e3ae70
Debkanta Samanta <debkanta484@gmail.com>
2024-12-03 20:34:29 +0530

    cors

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const colors = require("colors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+const connectDB = require("./config/db")
+const cors = require("cors")
 
 //dotenv config
 dotenv.config();
@@ -15,7 +16,7 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan('dev'));
-
+app.use(cors());
 
 //routes
 app.use('/api/v1/user', require('./routes/userRoutes'));
